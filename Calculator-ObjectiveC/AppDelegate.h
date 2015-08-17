e5ddcc12cdd1024abc711686333391576772cfdf
Anthony Walker <awalk91@hotmail.com>
2015-08-16 20:54:28 -0400

    Initial Commit

---
+++ Calculator-ObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator-ObjectiveC
+//
+//  Created by Anthony Walker on 8/16/15.
+//  Copyright (c) 2015 Anthony Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator-ObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator-ObjectiveC
+//
+//  Created by Anthony Walker on 8/16/15.
+//  Copyright (c) 2015 Anthony Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
